90f4807ce86e34549d8744eac403dc3e1a87c887
brughwani <brughwani@gmail.com>
2025-01-22 18:03:54 +0530

    resolving cors issue

--- api/fsdealerservice.js
+++ api/fsdealerservice.js
@@ -104,6 +104,17 @@ export async function GET(req) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+    if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  if (req.method !== 'GET') {
+    res.status(405).send('Method Not Allowed');
+    return;
+  }
+
+
 
 
     const { searchParams } = new URL(req.url);
--- api/fsproductservice.js
+++ api/fsproductservice.js
@@ -103,7 +103,17 @@ export async function GET(req) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+      if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
   
+    if (req.method !== 'GET') {
+      res.status(405).send('Method Not Allowed');
+      return;
+    }
+  
+
   
   const { searchParams } = new URL(req.url);
   const level = searchParams.get('level');
